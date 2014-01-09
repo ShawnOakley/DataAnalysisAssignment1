b4cc249a7c36d1cdb86413e9b9709d96f52a34ec
Shawn Oakley <shawnoakley@gmail.com>
2014-01-09 16:13:07 -0500

    Completes labels on Hinton graph.

--- hw1-c.py
+++ hw1-c.py
@@ -8,6 +8,7 @@ import numpy as np
 
 array = (eL.total_petroleum_stocks.T['United States'].values[3:]/10000 - .12 ) / .07
 
+
 matrix = array.reshape(6,5)
 
 
@@ -29,6 +30,18 @@ def hinton(matrix, max_weight=None, ax=None):
     ax.xaxis.set_major_locator(plt.NullLocator())
     ax.yaxis.set_major_locator(plt.NullLocator())
 
+    year = 1983
+    plt.title('U.S. Oil Reserves: 1983-2012', color='w', weight='bold')
+
+    # Subtitles in a graph
+    # http://stackoverflow.com/questions/1388450/giving-graphs-a-subtitle-in-matplotlib
+
+    plt.figtext(.5,.04,'Oil Reserves as measured in terms of Millions of Barrels ',fontsize=10,ha='center')
+    plt.figtext(.5,.01,'Red  < 40% of max capacity, Orange  < 70% of max capacity, Blue  < 90% of max capacity',fontsize=10,ha='center')
+
+    # Adding text to a rectangle
+    # http://stackoverflow.com/questions/14531346/how-to-add-a-text-into-a-rectangle
+
     for (x,y),w in np.ndenumerate(matrix):
     	if w < .4:
     		color = 'red'
@@ -39,12 +52,27 @@ def hinton(matrix, max_weight=None, ax=None):
     	else:
     		color = 'white'
         size = np.sqrt(np.abs(w))
-        rect = plt.Rectangle([x - size / 2, y - size / 2], size, size,
-                             facecolor=color, edgecolor=color)
+        # Plotting as rectangles
+        rect = plt.Rectangle([x - size / 2.5, y - size / 2], size, size,
+                             facecolor=color, edgecolor=color, label = "X")
         ax.add_patch(rect)
+
+        rx, ry = rect.get_xy()
+        cx = rx + rect.get_width()/2.0
+        cy = ry = rect.get_height()/2.0
+
+        ax.annotate(year, (x  - size/4, y + size/3), color='w', weight='bold')
+        year += 1
+        # Plotting as circles
+        # circle1=plt.Circle((w,w),.2,color=color)
+        # fig = plt.gcf()
+        # print fig
+        # print fig.gca()
+        # fig.gca().add_artist(circle1)
 
     ax.autoscale_view()
     ax.invert_yaxis()
+    ax.plot(label = "U.S. Oil Reserves")
 
 
 if __name__ == '__main__':
